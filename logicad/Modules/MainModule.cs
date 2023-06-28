0342c8f673f72740b35a659cef87baf1efff1bba
Airat <abdrakovairat@gmail.com>
2023-06-28 17:51:34 +0300

    First commit

---
+++ logicad/Modules/MainModule.cs
@@ -0,0 +1,23 @@
+﻿using logicad.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace logicad.Modules
+{
+    internal class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var region = containerProvider.Resolve<IRegionManager>();
+            // the view to display on start up
+            region.RegisterViewWithRegion("MainRegion", typeof(MainPageView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainPageView>();
+            containerRegistry.RegisterForNavigation<LogicPageView>();
+        }
+    }
+}
